9a83a59a58068faa27f48b7ee72a8f22d221a39f
Azwan Azlan <azwanazlan2809@gmail.com>
2022-08-26 09:45:55 +0800

    Update

--- index.js
+++ index.js
@@ -1,12 +1,74 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+
+const mongoose = require("mongoose");
+const TodoTask = require("./models/TodoTask");
+
+
+//connection to db
+
+//mongoose.set("useFindAndModify", false);
+
+dotenv.config();
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+
+    console.log("Connected to db!");
+
+    app.listen(3000, () => console.log("Server up and running"));
+});
 
 app.use("/static", express.static("public"));
 
+// Urlencoded will allow us to extract the data from the form by adding her to the body property of the request.
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 
-app.get('/',(req,res) => {
-    res.render('todo.ejs');
+// GET METHOD
+app.get("/", (req, res) => {
+    TodoTask.find({}, (err, tasks) => {
+    res.render("todo.ejs", { todoTasks: tasks });
+    });
+    });
+
+//POST METHOD
+app.post('/',async (req, res) => {
+    const todoTask = new TodoTask({
+            content: req.body.content
+        });
+    
+    try {
+        await todoTask.save();
+        res.redirect("/");
+    } catch (err) {
+        res.redirect("/");
+    }
+    });
+
+//UPDATE
+app
+.route("/edit/:id")
+.get((req, res) => {
+    const id = req.params.id;
+    TodoTask.find({}, (err, tasks) => {
+    res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+    });
+})
+.post((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+    if (err) return res.send(500, err);
+    res.redirect("/");
+    });
 });
 
-app.listen(3000, () => console.log("Server up and running"));
+//DELETE
+app.route("/remove/:id").get((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndRemove(id, err => {
+    if (err) return res.send(500, err);
+    res.redirect("/");
+    });
+    });
